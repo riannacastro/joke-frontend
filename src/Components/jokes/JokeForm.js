004853cc4b0ff0a9780438e87d7989993a95d5aa
Rianna Castro <riannaxcastro@gmail.com>
2021-12-09 09:31:13 -1000

    create handleChange method

--- src/Components/jokes/JokeForm.js
+++ src/Components/jokes/JokeForm.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export default class JokeForm extends Component {
+    state = {
+        joke: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+        
+    }
+
     render() {
         return (
             <form>
                 <label htmlFor="joke-input">Joke:</label>
-                <input id="joke-input" type="text" />
+                <input id="joke-input" onChange={this.handleChange} type="text" placeholder="Enter joke..." />
                 <input type="submit" />
             </form>
         )
