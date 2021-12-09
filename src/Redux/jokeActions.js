39d85387d7af601b2016d19dc9a27da929f71ccd
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 18:59:38 -1000

    added fetch

--- src/Redux/jokeActions.js
+++ src/Redux/jokeActions.js
@@ -1,1 +1,7 @@
-export const setJokes = (jokes) => ({type: "SET_JOKES", payload: jokes})
+export const setJokes = () => {
+    return (dispatch) => {
+        fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10")
+        .then(r => r.json())
+        .then(jokes => dispatch({type: "SET_JOKES", payload:jokes}))
+    }
+}
