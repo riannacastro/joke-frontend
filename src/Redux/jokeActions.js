8a87258471d8be36efe7f42a9f535c9899e2b556
Rianna Castro <riannaxcastro@gmail.com>
2021-12-09 09:08:50 -1000

    fetch working

--- src/Redux/jokeActions.js
+++ src/Redux/jokeActions.js
@@ -2,6 +2,6 @@ export const setJokes = () => {
     return (dispatch) => {
         fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10")
         .then(r => r.json())
-        .then(jokes => dispatch({type: "SET_JOKES", payload:jokes}))  // need to get to data.jokes
+        .then(jokes => dispatch({type: "SET_JOKES", payload:jokes.jokes}))  
     }
 }
