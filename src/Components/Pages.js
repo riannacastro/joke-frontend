6439f586f281849a30976057a90d2b74cb303a3b
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 17:08:20 -1000

    added switch/route

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import JokesContainer from './jokes/JokesContainer'
+
 
 export default function Pages() {
     return (
-        <div>
-            
-        </div>
+       <Switch>
+           <Route path="/jokes" component={routerProps => < JokesContainer routerProps={routerProps}/>}/>
+       </Switch>
     )
 }
