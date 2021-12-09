e6d4abc654112da06eb6f8fa16f3be61db07abf7
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 17:13:53 -1000

    added NavLink

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { NavLink } from 'react-router-dom';
 import Pages from './Components/Pages';
+
 
 function App() {
   return (
     <div className="App">
-      < Pages />
+      <NavLink to="/">Home</NavLink> 
+      <br />
+      <NavLink to="/jokes">Jokes</NavLink>
+      <Pages />
     </div>
   );
 }
