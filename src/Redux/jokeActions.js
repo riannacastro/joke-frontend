76fd850aed04a993da46aa277cbc478a797a2469
Rianna Castro <riannaxcastro@gmail.com>
2021-12-09 13:36:52 -1000

    changed fetch

--- src/Redux/jokeActions.js
+++ src/Redux/jokeActions.js
@@ -1,14 +1,14 @@
 export const setJokes = () => {
     return (dispatch) => {
-        fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10")
+        fetch("http://localhost:3000/jokes")
         .then(r => r.json())
-        .then(jokes => dispatch({type: "SET_JOKES", payload:jokes.jokes}))  
+        .then(jokes => dispatch({type: "SET_JOKES", payload:jokes}))  
     }
 }
 // fetch might need to be localhost 
 export const addJoke = (joke) => {
     return (dispatch) => {
-        fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10", {
+        fetch("http://localhost:3000/jokes", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
