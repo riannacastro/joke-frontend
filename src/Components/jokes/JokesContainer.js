26eb46f357443aedafc07530a56709d44840cb8e
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 18:52:54 -1000

    jokes showing

--- src/Components/jokes/JokesContainer.js
+++ src/Components/jokes/JokesContainer.js
@@ -7,13 +7,16 @@ class JokesContainer extends React.Component {
     componentDidMount() {
         fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10")
         .then(r => r.json())
-        .then(data => this.props.dispatchSetJokes(data))
+        .then(data => this.props.dispatchSetJokes(data.jokes))
     }
 
     render(){
         return (
             <div>
               <h1>ALL JOKES</h1>
+              <div>
+                  {this.props.jokes.map(j => <p key={j.id}>{j.joke}</p>)}
+              </div>
          </div>
         )
     }
