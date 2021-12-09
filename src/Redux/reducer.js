ac2673f4a1571aef9203a4d8724ceb6c08230454
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 16:55:30 -1000

    created reducer

---
+++ src/Redux/reducer.js
@@ -0,0 +1,12 @@
+function reducer(state = {}, action) {
+    switch (action.type) {
+        // case value:
+
+        //   break;
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
