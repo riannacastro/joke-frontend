6616c7f1abbad5f41fb4c2c47fd036fb4dd4d66a
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 18:30:51 -1000

    added mapDispatchToProps

--- src/Components/jokes/JokesContainer.js
+++ src/Components/jokes/JokesContainer.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setJokes } from './../../Redux/jokeActions'
 
-export default function JokesContainer() {
-    return (
-        <div>
-            <h1>ALL JOKES</h1>
-        </div>
-    )
+class JokesContainer extends React.Component {
+
+    componentDidMount() {
+        fetch("https://v2.jokeapi.dev/joke/Programming,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10")
+        .then(r => r.json())
+        .then(data => this.props.dispatchSetJokes(data))
+    }
+
+    render(){
+        return (
+            <div>
+              <h1>ALL JOKES</h1>
+         </div>
+        )
+    }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatchSetJokes: (jokes) => dispatch(setJokes(jokes))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(JokesContainer)
