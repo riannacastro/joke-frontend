2ebed4b82af1a53239b475b68239a67bb6dcd123
Rianna Castro <riannaxcastro@gmail.com>
2021-12-08 16:56:49 -1000

    import reducer to store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import reducer from "./reducer";
 
 export default createStore(reducer, applyMiddleware(thunk));
