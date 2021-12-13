3ea8a3a45eb75e1ad67a62e339d3a6509e2c7b38
Rianna Castro <riannaxcastro@gmail.com>
2021-12-13 12:25:27 -1000

    edited about

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export default class AboutPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
+            <div className="about-cont">
+                <h1>About the Jokester:</h1>
+                <p>We want you to take a little break from working so hard. Work can
+                    be stressful, so why don't we make it fun? Here are some programming jokes
+                    for you to either laugh at or cringe at. 
+
+                    If you would like to add a programming joke of your own, please go to the
+                    "new joke" page, and you will be able to enter your joke there.
+                </p>
             </div>
         )
     }
