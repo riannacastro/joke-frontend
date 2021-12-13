505637dc2c58892f0f2308fbeeb9e5f9c30ed2a5
Rianna Castro <riannaxcastro@gmail.com>
2021-12-13 12:03:18 -1000

    create how to file

---
+++ src/Components/HowToPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HowToPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
